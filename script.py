fa00cfb49c1c5ae8e4d431db7b220eee6fb36625
Matheus silva <103462698+Dev-Matheus-Silva@users.noreply.github.com>
2023-11-18 12:10:32 -0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# URL do site que você deseja abrir
+url = "https://otrs.grupolinhares.com.br/otrs/index.pl?Action=AgentTicketZoom;TicketID=87251"
+
+# Abre o site no navegador padrão
+webbrowser.open(url)
